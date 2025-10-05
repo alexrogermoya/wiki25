ee5dafdb6e34773bdf532c37b05c739c4beca8ad
alexrogermoya <alex.roger01@estudiant.upf.edu>
2025-10-05 23:45:17 +0200

    home final

--- static/home.js
+++ static/home.js
@@ -46,7 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let isBreaking = false;
         
         window.addEventListener('scroll', () => {
-            const scrollPercent = window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)*2;
+            const scrollPercent = window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)*10;
             const leftHalf = document.getElementById('leftHalf');
             const rightHalf = document.getElementById('rightHalf');
             const revealedText = document.getElementById('revealedText');
